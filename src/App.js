287f638a0ee7eef7fc1ba645c5fef3663f429a07
Suneel Chauhan <suneelms31@gmail.com>
2022-01-03 22:15:01 +0530

    frst cmt

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomepageComp from './components/HomepageComp';
+
+function App() {
+  return (
+    <div >
+      <HomepageComp/>
+    </div>
+  );
+}
+
+export default App;
